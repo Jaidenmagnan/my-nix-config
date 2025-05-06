051ff346e29d4b462ac31930f1944ead070c7d07
Jaiden Magnan <jaidenmagnan@gmail.com>
2025-05-06 14:39:06 -0400

    more defaults

--- apps/vscode.nix
+++ apps/vscode.nix
@@ -5,7 +5,7 @@
 
 	programs.vscode  = {
 		enable = true;
-	extensions = with pkgs.vscode-extensions; [
+	profiles.default.extensions = with pkgs.vscode-extensions; [
 		arcticicestudio.nord-visual-studio-code
 		vscodevim.vim
 		yzhang.markdown-all-in-one
