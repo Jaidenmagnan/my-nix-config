4cd1a48a21e6d9615992ca73dfbc687d54b7e967
Jaiden Magnan <jaidenmagnan@gmail.com>
2025-07-11 12:58:48 -0400

    catppuccin theme for ghostty

--- apps/ghostty.nix
+++ apps/ghostty.nix
@@ -1,7 +1,11 @@
-{config, lib, pkgs}:
+{config, lib, pkgs, ...}:
 {
  xdg.configFile."ghostty/config" = lib.mkForce {
     source = ./ghostty/config;
+  };
+
+ xdg.configFile."ghostty/catppuccin-mocha.conf" = lib.mkForce {
+    source = ./ghostty/catppuccin-mocha.conf;
   };
 
 
